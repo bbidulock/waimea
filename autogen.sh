f42c8a6e4d455c9a5dfcf17fc62e0400ac06221e
Brian Bidulock <bidulock@openss7.org>
2014-01-23 21:00:28 -0700

    better build system for github

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,24 @@
-#! /bin/sh
-set -x
-aclocal
-autoheader
-automake --copy --add-missing
-autoconf
+#!/bin/bash
+
+VERSION=
+REVISION=
+
+if [ -z "$VERSION" ]; then
+	VERSION='0.5.0'
+	if [ -x "`which git 2>/dev/null`" -a -d .git ]; then
+		REVISION=$(git describe --tags --long)
+		VERSION=$(git describe --tags|sed 's,[-_],.,g;s,\.g.*$,,')
+		(
+		   echo -e "# created with: git log --stat=76 | fmt -sct -w80\n"
+		   git log --stat=76 | fmt -sct -w80
+		)>ChangeLog
+	fi
+fi
+
+# if you don't have -i get a real version of sed!!!
+sed -r -e -i \
+    "s:AC_INIT\(.*:AC_INIT([waimea],[$VERSION],[https://github.com/bbidulock/waimea/issues]):
+     s:AC_REVISION\(.*:AC_REVISION([$REVISION]):" \
+    configure.ac
+
+autoreconf -fiv
